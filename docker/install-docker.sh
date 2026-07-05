c5c7edbaa9d52457e119f47e3f5e59aeca36072a
oqianlio <2428333244@qq.com>
2026-07-05 19:56:36 +0800

    修复：proxy-global 自动补全 http:// 协议前缀；install-docker.sh gpg 加 --batch 参数

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -35,7 +35,7 @@ fi
 if command -v apt &> /dev/null; then
     sudo apt update && sudo apt install -y ca-certificates curl gnupg lsb-release
     sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --no-tty --dearmor -o /etc/apt/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
 https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
